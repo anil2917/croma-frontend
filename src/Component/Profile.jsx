8042875a2fdd36dfaff2a0c35ec5136e7fcd048c
anil2917 <vsamrat274@gmail.com>
2025-05-26 16:59:52 +0530

    up

--- src/Component/Profile.jsx
+++ src/Component/Profile.jsx
@@ -30,18 +30,20 @@ const Profile = () => {
   return (
     <div
       style={{ width: "1" }}
-      className="w-full  bg-black p-4 sm:p-6 rounded-2xl bg-black text-white shadow-lg border-white-800 border w-[100%]"
+      className="   bg-black p-4 sm:p-6 rounded-2xl bg-black text-white shadow-lg   w-[100%]"
     >
-      <div className="text-xs sm:text-sm text-white mb-2">
+     
+      <div className="text-xs sm:text-sm text-white mb-2 bg-blue">
         My Account &gt; My Profile Page
-      </div>
-      <h1 className="text-xl sm:text-2xl font-bold mb-4">My Profile Page</h1>
-      <div className="w-[80%] flex justify-center m-auto">
-        <form action="" onSubmit={handleSubmit} className="flex flex-wrap">
-          <div className="mb-4 lg:w-[40%] w-[48%]">
-            <label className="block text-sm mb-1">Title</label>
+      </div> 
+      <div className="lg:w-[60%] lg:ml-55">
+      <h1 className="text-xl sm:text-2xl font-bold my-2 lg:ml-8">My Profile Page</h1>
+      <div className="flex justify-center m-auto">
+        <form action="" onSubmit={handleSubmit} className="lg:flex lg:flex-wrap   lg:w-full justify-center gap-3 ">
+          <div className="mb-4 lg:w-[45%] w-[100%] ">
+            <label className="block lg:text-[18px] text-sm  mb-1">Title</label>
             <select
-              className="w-full px-3 py-2 bg-black border  rounded-md focus:outline-none text-sm"
+              className="w-full px-3 py-2 bg-black border lg:h-12 rounded-md focus:outline-none text-sm"
               required
             >
               <option value=""></option>
@@ -52,46 +54,46 @@ const Profile = () => {
             </select>
           </div>
 
-          <div className="mb-4 lg:w-[40%] ">
-            <label className="block text-sm mb-1">First Name</label>
+          <div className="mb-4 lg:w-[45%] ">
+            <label className="block lg:text-[18px] text-sm mb-1">First Name</label>
             <input
               type="text"
               placeholder="Enter first Name"
               required
               onChange={handlechange}
               name="firstname"
-              className="w-full px-3 py-2 bg-white text-black  rounded-md focus:outline-none text-sm"
+              className="w-full lg:h-12  px-3 py-2 bg-white text-black  rounded-md focus:outline-none text-sm"
             />
           </div>
 
-          <div className="mb-4 lg:w-[40%] ">
-            <label className="block text-sm mb-1">Middle Name</label>
+          <div className="mb-4 lg:w-[45%] ">
+            <label className="block lg:text-[18px] text-sm mb-1">Middle Name</label>
             <input
               type="text"
               required
               onChange={handlechange}
               name="middlename"
               placeholder="Enter Middle Name"
-              className="w-full px-3 py-2 bg-white  text-black rounded-md focus:outline-none text-sm"
+              className="w-full lg:h-12 px-3 py-2 bg-white  text-black rounded-md focus:outline-none text-sm"
             />
           </div>
 
-          <div className="mb-4 lg:w-[40%] ">
-            <label className="block text-sm mb-1">Last Name</label>
+          <div className="mb-4 lg:w-[45%] ">
+            <label className="block lg:text-[18px] text-sm mb-1">Last Name</label>
             <input
               type="text"
               placeholder="Enter Last Name"
               required
               onChange={handlechange}
               name="lastname"
-              className="w-full px-3 py-2 bg-white text-black rounded-md focus:outline-none text-sm"
+              className="w-full  lg:h-12 px-3 py-2 bg-white text-black rounded-md focus:outline-none text-sm"
             />
           </div>
 
-          <div className="mb-4 ">
-            <label className="block text-sm mb-1">Gender</label>
-            <div className="flex   sm:gap- gap-4 text-sm h-auto  flex-wrap">
-              <label className="flex items-center gap-2">
+          <div className="mb-4 lg:ml-18 w-full ">
+            <label className="block lg:text-[18px] text-sm mb-1 ">Gender</label>
+            <div className="flex   sm:gap- gap-4 text-m h-auto  flex-wrap">
+              <label className="flex items-center gap-2 ">
                 <input
                   type="radio"
                   name="gender"
@@ -99,9 +101,9 @@ const Profile = () => {
                   value={"female"}
                   className="accent-[#12daa8] "
                 />{" "}
-                Female
+                 Female
               </label>
-              <label className="flex items-center gap-2">
+              <label className="flex items-center gap-2 pl-5">
                 <input
                   type="radio"
                   name="gender"
@@ -109,19 +111,19 @@ const Profile = () => {
                   value={"male"}
                   className="accent-[#12daa8]"
                 />{" "}
-                Male
+                 Male
               </label>
-              <label className="flex items-center gap-2">
+              <label className="flex items-center gap-2 pl-5">
                 <input
                   type="radio"
                   name="gender"
                   onChange={handlechange}
                   value={"Transgender"}
-                  className="accent-[#12daa8]"
+                  className="accent-[#12daa8] "
                 />{" "}
-                Transgender
+                 Transgender
               </label>
-              <label className="flex items-center gap-2">
+              <label className="flex items-center gap-2 pl-5">
                 <input
                   type="radio"
                   name="gender"
@@ -129,15 +131,15 @@ const Profile = () => {
                   value={"I'd rather not say"}
                   className="accent-[#12daa8]"
                 />{" "}
-                I'd rather not say
+                 I'd rather not say
               </label>
             </div>
           </div>
 
-          <div className="mb-4 lg:w-[40%] ">
-            <label className="block text-sm mb-1">Mobile number</label>
+          <div className="mb-4 lg:w-[45%] ">
+            <label className="block lg:text-[18px] text-sm mb-1">Mobile number</label>
             <div className="flex">
-              <span className="bg-white text-black px-3 py-2 rounded-l-md text-sm">
+              <span className="bg-white text-black lg:h-12 px-3 py-2 rounded-l-md text-sm">
                 +91
               </span>
               <input
@@ -146,48 +148,49 @@ const Profile = () => {
                 value={defaultNumber}
                 disabled
                 onChange={handlechange}
-                className="w-full  px-3 py-2 bg-white text-black   border-l-black rounded-r-md focus:outline-none text-sm"
+                className="w-full  lg:h-12 px-3 py-2 bg-white text-black   border-l-black rounded-r-md focus:outline-none text-sm"
               />
             </div>
           </div>
 
-          <div className="mb-4 lg:w-[40%] ">
-            <label className="block text-sm mb-1">Email Id</label>
+          <div className="mb-4 lg:w-[45%] ">
+            <label className="block lg:text-[18px] text-sm mb-1">Email Id</label>
             <input
               type="email"
               name="email"
               onChange={handlechange}
-              className="w-full px-3 py-2 bg-white text-black rounded-md focus:outline-none text-sm"
+              className="w-full px-3 lg:h-12 py-2 bg-white text-black rounded-md focus:outline-none text-sm"
             />
           </div>
 
-          <div className="mb-4 lg:w-[40%] ">
-            <label className="block text-sm mb-1">Date of birth</label>
+          < div className="mb-4 lg:w-[45%] ">
+            <label className="block lg:text-[18px] text-sm mb-1">Date of birth</label>
             <input
               type="date"
               name="dob"
               required
               onChange={handlechange}
-              className="w-full px-3 py-2 bg-white text-black rounded-md focus:outline-none text-sm"
+              className="w-full lg:h-12 px-3 py-2 bg-white text-black rounded-md focus:outline-none text-sm"
             />
-          </div>
-          <div className="mb-4 lg:w-[40%] ">
-            <label className="block text-sm mb-1">Date of Anniversary</label>
+          </ div>
+          <div className="mb-4 lg:w-[45%] ">
+            <label className="block lg:text-[18px] text-sm mb-1">Date of Anniversary</label>
             <input
               type="date"
               required
               onChange={handlechange}
               name="doa"
-              className="w-full px-3 py-2 bg-white text-black rounded-md focus:outline-none text-sm"
+              className="w-full  lg:h-12 px-3 py-2 bg-white text-black rounded-md focus:outline-none text-sm"
             />
-          </div>
+          </ div>
           <button
-            className="bg-[#12daa8] w-[130px] h-10 rounded-[7px] mx-30 mt-5 "
+            className="bg-[#12daa8] w-[130px] h-10  rounded-[7px] mx-30 mt-5 "
             onClick={handleSubmit}
           >
             Save Change
           </button>
         </form>
+      </div>
       </div>
     </div>
   );
